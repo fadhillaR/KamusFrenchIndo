6e6355ab4951b8fda41c125943179129a5f12cd5
unknown <fahaara166@gmail.com>
2024-03-08 18:43:26 +0700

    update aplikasi

--- lib/screen_page/search_screen.dart
+++ lib/screen_page/search_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:kamusfrench_indo/screen_page/detail_screen.dart';
 // import 'package:kamusfrench_indo/models/model_kata.dart';
 
 class PageListKata extends StatefulWidget {
